2c91ede28df2ca3996c79ea50ef1af9be06a0365
FerMod <tudelaigarcia@gmail.com>
2019-11-16 13:46:56 +0100

    Add scripts to open only one cmd console

---
+++ run_single_cmd.bat
@@ -0,0 +1,8 @@
+@echo off
+
+@cd /D %~dp0
+@cd bin\mas
+
+start /B jgomas_manager.bat
+timeout 5 > nul
+start /B jgomas_launcher.bat
---
+++ run_with_render_single_cmd.bat
@@ -0,0 +1,12 @@
+@echo off
+
+@cd /D %~dp0
+@cd bin\mas
+
+start /B jgomas_manager.bat
+timeout 5 > nul
+start /B jgomas_launcher.bat
+
+@cd ..\render\w32\
+timeout 5 > nul
+start /B run_jgomasrender.bat
