a9b934191af3d39f9fec1383030044827ea7d6bc
FerMod <tudelaigarcia@gmail.com>
2019-11-26 19:04:20 +0100

    Add independent config

--- bin/mas/agents/default/jgomas_manager.bat
+++ bin/mas/agents/default/jgomas_manager.bat
@@ -1,9 +1,8 @@
-
-set MAS_PATH="..\..\"
+@echo off 
 
 set NUM_AGENTS=14
-set MAP="map_04"
+set MAP=map_04
 set REFRESH_RATE=125
 set MATCH_TIME=10 
 
-java -classpath "%MAS_PATH%lib\jade.jar;%MAS_PATH%lib\jadeTools.jar;%MAS_PATH%lib\Base64.jar;%MAS_PATH%lib\http.jar;%MAS_PATH%lib\iiop.jar;%MAS_PATH%lib\beangenerator.jar;%MAS_PATH%lib\jgomas.jar;%MAS_PATH%lib\jason.jar;%MAS_PATH%lib\JasonJGomas.jar;%MAS_PATH%classes;%MAS_PATH%." jade.Boot -gui "Manager:es.upv.dsic.gti_ia.jgomas.CManager(%NUM_AGENTS%, %MAP%, %REFRESH_RATE%, %MATCH_TIME%)"
+java -classpath "lib\jade.jar;lib\jadeTools.jar;lib\Base64.jar;lib\http.jar;lib\iiop.jar;lib\beangenerator.jar;lib\jgomas.jar;lib\jason.jar;lib\JasonJGomas.jar;classes;." jade.Boot -gui "Manager:es.upv.dsic.gti_ia.jgomas.CManager(%NUM_AGENTS%, %MAP%, %REFRESH_RATE%, %MATCH_TIME%)"
--- run_single_cmd.bat
+++ run_single_cmd.bat
@@ -1,13 +1,17 @@
 @echo off
 cls
 
-set AGENTS_PATH="agents\default\"
-rem set AGENTS="agents\task"
-rem set AGENTS="agents\task5"
+set AGENTS=default
+rem set AGENTS=task
+rem set AGENTS=task5
 
 @cd /D %~dp0
 @cd bin\mas
 
-start /B "%AGENTS_PATH%\jgomas_manager.bat"
+start /B agents\%AGENTS%\jgomas_manager.bat agents\%AGENTS%
 timeout 5 > nul
-start /B "%AGENTS_PATH%\jgomas_launcher.bat"
+start /B agents\%AGENTS%\jgomas_launcher.bat agents\%AGENTS%
+
+cd ..\render\w32\
+timeout 5 > nul
+start /B run_jgomasrender.bat
