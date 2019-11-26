0841e82bc0637abeed18a417d38f2bc36fcfa90c
FerMod <tudelaigarcia@gmail.com>
2019-11-26 22:16:27 +0100

    Add folders with different agent setup

--- run.bat
+++ run.bat
@@ -1,9 +1,19 @@
 @echo off
 cls
 
-cd /D %~dp0
-cd bin\mas
+set AGENTS=default
+rem set AGENTS=task5
+rem set AGENTS=tasks
 
-start /B jgomas_manager.bat
+set AGENTS_PATH=.\agents\%AGENTS%
+
+@cd /D %~dp0
+@cd .\bin\mas
+
+start /B %AGENTS_PATH%\jgomas_manager.bat %AGENTS_PATH%
 timeout 5 > nul
-start jgomas_launcher.bat
+start /B %AGENTS_PATH%\jgomas_launcher.bat %AGENTS_PATH%
+
+cd ..\render\w32\
+timeout 5 > nul
+start /B run_jgomasrender.bat
